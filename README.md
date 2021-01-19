2955b67002a451aee9de26a2a3671e570ddbc23e
vishwampandya <vishwampandya00@gmail.com>
2021-01-19 23:30:43 +0530

    modified script so it can run from anywhere

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 2)Give sample input for all templates and proper comment it (all the types of inputs...)  
 3)set input as var for all templates (specifically buffer reader one)</br>  
 
-###BUGS  
-1)If run the script from test folder , its not working (make it such that it can run from any where)  
+###For read me  
+1)added a templates_path variable for temporary and file execution  
+2)add proper steps to set home dierectory path
+
+###Bugs
--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
+#templates_path=$HOME/bin/CP_Java_template/templates
+#below one for testing
+templates_path=$HOME/E/Projects/CP_Java_template/templates
+
 echo "enter file name"
 read filename
 full_filename=$filename".java"
 touch $full_filename
-sed 's/MainClass/'$filename'/g' templates/template_4_Scanner_class.txt > $full_filename
+sed 's/MainClass/'$filename'/g' $templates_path/template_4_Scanner_class.txt > $full_filename
 echo "file created!"
