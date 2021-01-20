1e442796748b8e0cf072667be5b661b58616a6ac
vishwampandya <vishwampandya00@gmail.com>
2021-01-21 00:09:08 +0530

    Modified , so can now read as cmd argument

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 1)obviously , make this readme file  
 2)Give sample input for all templates and proper comment it (all the types of inputs...)  
 3)set input as var for all templates (specifically buffer reader one)</br>  
+4)do something for default values if - no arguments passed , first passed but isnumber and vice versa , number out of range etc  
 
 ###For read me  
 1)added a templates_path variable for temporary and file execution  
-2)add proper steps to set home dierectory path
+2)add proper steps to set home dierectory path  
 
 ###Bugs
--- run.sh
+++ run.sh
@@ -1,12 +1,31 @@
+
 #!/bin/bash
 
 #templates_path=$HOME/bin/CP_Java_template/templates
 #below one for testing
 templates_path=$HOME/E/Projects/CP_Java_template/templates
 
-echo "enter file name"
-read filename
+filename=$1
+
+if [[ $# == 1 ]] || [[ $2 == 4 ]]
+then
+ template_name=template_4_Scanner_class.txt
+elif [[ $2 == 1 ]]
+then
+ template_name=template_1_Reader_class.txt
+elif [[ $2 == 2 ]]
+then
+ template_name=template_2_FastReader_class.txt
+elif [[ $2 == 3 ]]
+then
+ template_name=template_3_BufferReader_class.txt
+else
+ echo "Enter between 1 to 4"
+fi
+
 full_filename=$filename".java"
 touch $full_filename
-sed 's/MainClass/'$filename'/g' $templates_path/template_4_Scanner_class.txt > $full_filename
+sed 's/MainClass/'$filename'/g' $templates_path/$template_name > $full_filename
 echo "file created!"
+
+#add if exist then file created , other wise not
