da1728d23a5ab76872e7c76e6c9f2f386199dff1
vishwampandya <vishwampandya00@gmail.com>
2021-05-03 16:52:00 +0530

    Renamed to proper names

--- detect.sh
+++ detect.sh
@@ -1,7 +1,7 @@
 for((i = 1; ; ++i)); do
     echo "Checking Testcase : "$i
     ./TestingSolution/gen $i > ./TestingSolution/genIn
-     ./pipe.out $1 $2
+     ./detect.out $1 $2
      diff -w ./TestingSolution/out1 ./TestingSolution/out2 > diffCommandOutput.txt || break
     
 done
@@ -13,11 +13,11 @@ echo "For testcase "$i" : "
 cat ./TestingSolution/genIn
 printf "\n\n"
 
-echo "program 1 returned : "
+echo "program $1 returned : "
 cat ./TestingSolution/out1
 printf "\n\n"
 
-echo "program 2 returned : "
+echo "program $2 returned : "
 cat ./TestingSolution/out2
 printf "\n\n"
 echo "--------------------------------------------------------------------------------------------------------"
